010a8b85303976a655dec21935fc1b06200d28b3
jakub-siwiec <jakub.siwiec@svcais.com>
2021-09-07 22:57:51 +0100

    Added an initail version of identity

---
+++ pages/api/get-identity.js
@@ -0,0 +1,16 @@
+import { parseCookies } from 'nookies'
+
+export default async function handler(req, res) {
+    const cookies = parseCookies({req})
+
+    if (cookies.accesstoken) {
+        const response = await fetch('http://localhost:8002/identity', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + cookies.accesstoken,
+            },
+        })
+        const data = await response.json()
+        res.status(200).json(data)
+    }
+}  
