5de834b4aba86dcd3af1ed3758c10347a9792733
jakub-siwiec <jakub.siwiec@svcais.com>
2021-09-26 01:41:42 +0100

    Index page with link and icon

--- components/login/BankBox.js
+++ components/login/BankBox.js
@@ -1,20 +1,39 @@
-import useSWR from 'swr'
+import Link from 'next/link'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import swrRequest from '../../libs/swrRequest'
+
+import FullSiteCentered from '../FullSiteCentered'
 import LoginPlaid from './LoginPlaid'
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
-
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 const BankBox = () => {
 
-    const { data: institution, error } = useSWR('/api/get-institution', fetcher)
+    const { data: institution, error } = swrRequest('/api/get-institution')
 
     const nameExists = institution && institution.institution
 
+    console.log(institution)
+
     return nameExists ? (
-        <div>
-            {institution.institution.name}
-        </div>
+        <FullSiteCentered>
+            <Link href='/accounts'>
+                <div className="card">
+                    <header className="card-header">
+                        <p class="card-header-title">
+                            {institution.institution.name}
+                        </p>
+                        <button class="card-header-icon">
+                            <span class="icon">
+                                <FontAwesomeIcon icon={faAngleRight} />
+                            </span>
+                        </button>
+                    </header>   
+                </div>
+            </Link>
+        </FullSiteCentered>
     ) : <LoginPlaid />
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.scss'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 import { useEffect } from 'react'
 
 import { withRouter } from 'next/router'
