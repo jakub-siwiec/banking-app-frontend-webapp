6ebf42b967660e715756f156f5c096879b1d17df
jakub-siwiec <jakub.siwiec@svcais.com>
2021-10-03 23:46:55 +0100

    Initial delete cookie

--- pages/api/delete-access-token.js
+++ pages/api/delete-access-token.js
@@ -1,10 +1,37 @@
+import { parseCookies, destroyCookie } from 'nookies'
+
 import cookiesRequest from '../../libs/cookiesRequest'
 
 export default async function handler(req, res) {
-    await cookiesRequest(
-        req,
-        res,
-        'http://localhost:8002/access-token',
-        'DELETE'
-    )
+    // await cookiesRequest(
+    //     req,
+    //     res,
+    //     'http://localhost:8002/access-token',
+    //     'DELETE'
+    // )
+
+    const cookies = parseCookies({req})
+    const address = 'http://localhost:8002/access-token'
+    const method = 'DELETE'
+
+    if (cookies.accesstoken) {
+        console.log(cookies.accesstoken)
+        const response = await fetch(address, {
+            method: method,
+            headers: {
+                'Authorization': 'Bearer ' + cookies.accesstoken,
+            },
+        })
+        const data = await response.json()
+        destroyCookie({ res }, 'accesstoken')
+        res.status(200).json(data)
+    } else {
+        res.status(401).json({
+            status_code: 401,
+            error_code: "No token received"
+        })
+    }
+
+
+    
 }  
