e02ab7a010a808e730b11791a5712f911abfe936
jakub-siwiec <jakub.siwiec@svcais.com>
2021-10-25 22:43:03 +0100

    Link token

--- pages/api/link-token.js
+++ pages/api/link-token.js
@@ -1,11 +1,10 @@
+import backendRequest from '../../libs/requests/backendRequest'
+
+
 export default async function handler(req, res) {
-    try {
-        const response = await fetch('http://localhost:8002', {
-            method: 'GET',
-        })
-        const data = await response.json()
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+    await backendRequest(
+        req, 
+        res, 
+        'http://localhost:8002'
+    )
 }
