d911cec1d4998ad859f5b8fb7a9229643dc358cd
jakub-siwiec <jakub.siwiec@svcais.com>
2021-10-27 11:45:42 +0100

    Product not ready initial handling

--- components/account/TransactionsTable.js
+++ components/account/TransactionsTable.js
@@ -14,8 +14,11 @@ const TransactionsTable = () => {
 
     const { data: dataTransactions, error: errorTransactions, loading: loadingTransactions } = swrRequest(`/api/transactions/${accountId}`)
 
+    if (
+        loadingTransactions 
+        || (errorTransactions && errorTransactions.code === 'PRODUCT_NOT_READY')
+    ) return <Loader />
     if (errorTransactions) return <ErrorItem errorStatus={errorTransactions.status} errorText={errorTransactions.statusText} address={`/api/transactions/${accountId}`} />
-    if (loadingTransactions) return <Loader />
 
     return (
         <table className="table">
--- libs/requests/swrRequest.js
+++ libs/requests/swrRequest.js
@@ -1,18 +1,22 @@
+import { startsWith } from 'lodash'
 import useSWR from 'swr'
 
 
 export default function swrRequest(url) {
     const fetcher = (...args) => fetch(...args).then(async res => {
         const response = await res.json()
-        if (response.status_code < 200 || response.status_code >= 300) {
-            const error = new Error(`There was an error: ${response.status_code} ${response.error_code}`)
+        if (!res.ok) {
+            const error = new Error(`There was an error: ${response.status_code} ${response.message}`)
             error.info = response
             error.status = response.status_code
-            error.statusText = response.error_code
+            error.message = response.message
+            error.code = response.code
+            error.type = response.type
             throw error
         }
         return response
     })
+
     const { data, error } = useSWR(url, fetcher)
 
     return {
